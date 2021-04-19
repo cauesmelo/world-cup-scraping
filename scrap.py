8ba18faf6323ad5eb3fa1b662b59039510aec9e7
Cauê Melo <cauesmelo@gmail.com>
2021-04-19 14:43:15 -0300

    add header

---
+++ scrap.py
@@ -0,0 +1,8 @@
+__author__ = "Cauê Melo, Leone Daher"
+__copyright__ = "Copyright 2007, The Cogent Project"
+__credits__ = ["Cauê Melo", "Leone Daher", "Morganna Carmem Diniz"]
+__license__ = "MIT"
+__version__ = "0.0.1"
+__maintainer__ = "Cauê Melo"
+__email__ = "caue.melo@uniriotec.br"
+__status__ = "Development"
