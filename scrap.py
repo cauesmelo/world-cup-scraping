21e7be04490d24f8b397ce77b0d64172b3d4e162
caue <caue@MacBook-Pro-de-Monica.local>
2021-05-05 23:21:16 -0300

    add request

--- scrap.py
+++ scrap.py
@@ -1,8 +1,16 @@
-__author__ = "Cauê Melo, Leone Daher"
+__author__ = "Cauê Melo"
 __copyright__ = "Copyright 2007, The Cogent Project"
-__credits__ = ["Cauê Melo", "Leone Daher", "Morganna Carmem Diniz"]
+__credits__ = ["Cauê Melo", "Morganna Carmem Diniz"]
 __license__ = "MIT"
 __version__ = "0.0.1"
 __maintainer__ = "Cauê Melo"
 __email__ = "caue.melo@uniriotec.br"
 __status__ = "Development"
+
+from bs4 import BeautifulSoup
+import requests
+
+page = BeautifulSoup(requests.get("https://www.fifa.com/worldcup/").content, 'html.parser')
+
+#capturar links das copas
+print(page)
